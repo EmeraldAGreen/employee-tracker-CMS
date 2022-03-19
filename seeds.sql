cca36f6e5ff8db62c401de850a29ab19074faffc
Emerge <egreengirl1@gmail.com>
2022-03-19 17:11:22 -0600

    installed npm packages, created sql files

---
+++ seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO department (name)
+VALUES ("Human Resources"),
+       ("Marketing"),
+       ("Customer Service"),
+       ("Accounting");
+
+INSERT INTO role (title, salary)
+VALUES ("Manager", 55000),
+       ("Director", 82000),
+       ("Intern", 37000);
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("Toby", "Flenderson"),
+       ("Darryl","Philbin"),
+       ("Kelly", "Kapoor"),
+       ("Kevin", "Malone"),
+       ("Oscar", "Martinez"),
+       ("Phyllis", "Vance"),
+       ("Angela", "Martin");
