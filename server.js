446b2ce3e0c2de1d1aeabf56c4121761dea03c5f
Emerge <egreengirl1@gmail.com>
2022-03-20 19:35:39 -0600

    comment out bluebird promise template

--- server.js
+++ server.js
@@ -68,35 +68,15 @@ function startMenu() {
 }
 
 // VIEW 
-function viewAllDepartments() {
-// query database
-const [first_name, last_name, role_id, manager_id] = await db.execute('SELECT * FROM `employees`');
-}
-
-function viewEmployee() {
-	console.log("Employee Rota:\n");
-
-	var query = `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
-  FROM employee e
-  LEFT JOIN role r
-	ON e.role_id = r.id
-  LEFT JOIN department d
-  ON d.id = r.department_id
-  LEFT JOIN employee m
-	ON m.id = e.manager_id`;
-
-	connection.query(query, function (err, res) {
-		if (err) throw err;
-
-		console.table(res);
-		console.log("\n<<<<<<<<<<<<<<<<<<<< ⛔ >>>>>>>>>>>>>>>>>>>>\n");
-
-		firstPrompt();
-	});
-}
-
-
-
+// function viewAllDepartments() {
+   
+    // query database
+// const [rows, fields] = await db.execute('SELECT * FROM `table` WHERE `name` = ? AND `age` > ?', ['Morty', 14], 
+// ['Rick C-137', 53],
+// function(err, results, fields) {
+//   console.log(results); // results contains rows returned by server
+//   console.log(fields);
+// });
 
 // ADD
 
