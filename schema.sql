182decfa79e1879150d8cb0434069437b0231413
Emerge <egreengirl1@gmail.com>
2022-03-20 20:10:48 -0600

    fixed schema and seeds syntax errors

--- schema.sql
+++ schema.sql
@@ -4,31 +4,28 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE departments (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   department_name VARCHAR(30),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (departments_id)
-        REFERENCES department(id)
+    FOREIGN KEY (department_id)
+        REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
     first_name VARCHAR(30), 
     last_name VARCHAR(30), 
     role_id INT NOT NULL,
     FOREIGN KEY (role_id)
-        REFERENCES roles(id), 
-    manager_id INT
-    FOREIGN KEY (manager_id)
-        REFERENCES employees(id)
-    
+    REFERENCES roles(id), 
+    manager_id INT REFERENCES employees(id)
 );
--- seeds.sql
+++ seeds.sql
@@ -1,15 +1,10 @@
-INSERT INTO department (name)
-VALUES 
--- 1
-       ("Human Resources"),
--- 2
+INSERT INTO departments (department_name)
+VALUES ("Human Resources"),
        ("Marketing"),
--- 3
        ("Accounting"),
--- 4
        ("Sales");
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES ("Manager", 50000, 1),
        ("Manager", 50000, 3), 
        ("Manager", 54000, 4),
@@ -18,19 +13,17 @@ VALUES ("Manager", 50000, 1),
        ("Intern", 37000, 4),
        ("Intern", 48000, 3);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES 
---Manager of Human Resources
-       ("Toby", "Flenderson", 1, NULL),
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Toby", "Flenderson", 1, NULL),
 -- Manager of Accounting
        ("Oscar", "Martinez", 1, 5),
 -- Manager of Sales
        ("Phyllis", "Vance", 1, NULL), 
---Director of Marketing
+-- Director of Marketing
        ("Darryl","Philbin", 2, NULL),
 -- Director of Accounting
        ("Angela", "Martin", 2, NULL),
---Intern of Sales
+-- Intern of Sales
        ("Kelly", "Kapoor", 2, 4),
 -- Intern of Accounting
        ("Kevin", "Malone", 2, 2);
