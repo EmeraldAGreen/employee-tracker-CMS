e02cab2c1f4b15624d5f944ceeab38a1f5b48a42
Emerge <egreengirl1@gmail.com>
2022-03-21 22:56:00 -0600

    all ADD and VIEW functions working

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express');
 const inquirer = require('inquirer')
 const consoleTable = require('console.table')
 
-// Prompts
-const prompt = require("./prompts");
-
 // Import and require mysql2
 const mysql2 = require('mysql2');
 // get the promise implementation, we will use bluebird
@@ -47,7 +44,7 @@ function startMenu() {
             "View All Employees",
             "Add A Department",
             "Add A Role",
-            "Add A Employee",
+            "Add An Employee",
             "Update an Employee Role",
             "Exit",
           ]
@@ -69,7 +66,7 @@ function startMenu() {
                 case "Add A Role":
                     addRole();
                     break;
-                case "Add A Employee":
+                case "Add An Employee":
                     addEmployee();
                     break;
                 case "Update an Employee Role":
@@ -85,7 +82,7 @@ function startMenu() {
         })
 };
 
-// VIEW
+// VIEW (3)
 // Function to view all departments
 function viewAllDepartments() {
   var query = `SELECT department_name AS Departments FROM departments;`;
@@ -100,7 +97,6 @@ function viewAllRoles() {
     var query = `SELECT title AS Roles, salary AS Salaries FROM roles;`
     db.query(query, function(err, query){
       console.table(query);
- 
     });     
 startMenu();
   };
@@ -114,12 +110,160 @@ function viewAllEmployees() {
     });
 startMenu();
 };
-// ADD
 
+// ADD (3)
+function addDepartment() {
+  inquirer
+    .prompt([
+    {
+      name: "new_department",
+      type: "input",
+      message: "Enter new department's name:"
+    }
+  ])
+  .then(function(answer) {
+    db.query(
+      "INSERT INTO departments SET ?",
+      {
+        department_name: answer.new_department
+      },
+      function(err) {
+        if (err) throw err;
+        console.log(`Your new department ${answer.new_department} has been added`);
+        viewAllDepartments();
+        startMenu();
+      }
+    );
+  });
+};
 
+//Function to new add role
+function addRole() {
+  let existingDepartments = [];
+    db.query("SELECT * FROM departments", function(err, resDept) {
+      if (err) throw err;
+      for (var i = 0; i < resDept.length; i++) {
+        var deptList = resDept[i].department_name;
+        existingDepartments.push(deptList);
+        
+    }
+  inquirer
+  .prompt([
+  {
+    name: "title",
+    type: "input",
+    message: "What new role would you like to add?"
+  },
+  {
+    name: "salary",
+    type: "number",
+    message: "Enter new role's salary:"
+  },
+  {
+    name: "new_department_name",
+    type: "list",
+    message: "Which department does this role belong in?",
+    choices: existingDepartments
+  }
+])
+.then(function(answer) {
+  var chosenDepartment;
+        for (var i = 0; i < resDept.length; i++) {
+          if (resDept[i].department_name === answer.new_department_name) {
+            chosenDepartment = resDept[i];
+          }
+        };
+  db.query(
+    "INSERT INTO roles SET ?",
+    {
+      title: answer.title,
+      salary:answer.salary,
+      department_id: chosenDepartment.id
+    },
+    function(err) {
+      if (err) throw err;
+      console.log(`Your new role ${answer.title} has been added successfully!`);
+      viewAllRoles();
+      startMenu();
+    }
+  );
+});
+})
+};
 
+function addEmployee() {
+    //array to display prompt choices from database items 
+    var existingRoles = [];
+    db.query("SELECT * FROM roles", function (err, resRole) {
+        if (err) throw err;
+        for (var i = 0; i < resRole.length; i++) {
+            var roleList = resRole[i].title;
+            existingRoles.push(roleList);
+        }
+        // select from all of the employees; could all have a manager_id;
+        var existingEmployees = [];
+    db.query("SELECT * FROM employees", function (err, resEmployees) {
+        if (err) throw err;
+        for (var i = 0; i < resEmployees.length; i++) {
+            var employeeList = `${resEmployees[i].first_name} ${resEmployees[i].last_name}`;
+            existingEmployees.push(employeeList);
+        }
+    inquirer
+      .prompt([
+      {
+        name: "first_name",
+        type: "input",
+        message: "Enter employee's first name:"
+      },
+      {
+        name: "last_name",
+        type: "input",
+        message: "Enter employee's last name:"
+      },
+      {
+        name: "role_name",
+        type: "list",
+        message: "Select employee role:",
+        choices: existingRoles
+      },
+      {
+        name: "manager_id",
+        type: "list",
+        message: "Select the new employee's Manager or Director:",
+        choices: existingEmployees
+      }
+    ])
+      .then(function(answer) {
+        //for loop to retun 
+        var chosenRole;
+          for (var i = 0; i < resRole.length; i++) {
+            if (resRole[i].title === answer.role_name) {
+              chosenRole = resRole[i];
+            }
+          };
 
-// UPDATE
+        //connection to insert response into database  
+        db.query(
+          "INSERT INTO employees SET ?",
+          {
+            first_name: answer.first_name,
+            last_name: answer.last_name,
+            role_id: chosenRole.id,
+            manager_id: chosenRole.manager_id
+          },
+          function(err) {
+            if (err) throw err;
+            console.log(`Your new employee, ${answer.first_name} ${answer.last_name} has been added successfully!`);
+            viewAllEmployees();
+            startMenu();
+          }
+        );
+      })
+    });
+})};
+
+// UPDATE (1)
+
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
